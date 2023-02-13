1d68224e5f13fd0ed280d356675622e5cca8fd13
OneEpitome <rkddlfah0319@gmail.com>
2023-02-13 17:06:23 +0900

    Update Review.js
    리뷰 작성자가 아니라면 수정 삭제 버튼이 보이지 않도록 설정

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
@@ -7,18 +8,35 @@ function Review({ review, getReviewList }) {
     getReviewList();
   };
 
+  const [userId, setUserId] = useState(null);
+  useEffect(() => {
+    const userIdSetting = async () => {
+      setUserId(await (await fetch("/api/userId")).text());
+    };
+    userIdSetting();
+  }, []);
+
   return (
     <>
       <h1>{review.title}</h1>
       <h2>{review.content}</h2>
       <h3>{review.creatorId}</h3>
-      <img src={"http://localhost:8080/images/" + review.fileName} />
-      <Link to={"/edit/review/" + review.id}>
-        <Button variant="primary">리뷰 수정</Button>
-      </Link>
-      <Button variant="primary" onClick={onClickDeleteBtn}>
-        리뷰 삭제
-      </Button>
+      <img
+        src={"http://localhost:8080/images/" + review.fileName}
+        width="100px"
+        height="50px"
+      />
+      {userId == review.creatorId ? (
+        <Link to={"/edit/review/" + review.id}>
+          <Button variant="primary">리뷰 수정</Button>
+        </Link>
+      ) : null}
+
+      {userId == review.creatorId ? (
+        <Button variant="primary" onClick={onClickDeleteBtn}>
+          리뷰 삭제
+        </Button>
+      ) : null}
     </>
   );
 }
